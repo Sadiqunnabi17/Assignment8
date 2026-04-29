89cc8a59294a07cbefb3b291b4c4fbc14c257f2e
Sadiqunnabi17 <sadiqunnabi@gmail.com>
2026-04-29 23:18:20 +0600

    Project Setup

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="bg-base-200 p-6 mt-10 text-center">
+      <p>© 2026 SkillSphere. All rights reserved.</p>
+
+      <div className="flex justify-center gap-4 mt-2">
+        <a href="#">Privacy Policy</a>
+        <a href="#">Terms</a>
+      </div>
+    </footer>
+  );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <div className="navbar bg-base-100 shadow-md px-6">
+      {/* Left */}
+      <div className="flex-1">
+        <Link href="/" className="text-xl font-bold">
+          SkillSphere
+        </Link>
+      </div>
+
+      {/* Center */}
+      <div className="hidden md:flex gap-6">
+        <Link href="/">Home</Link>
+        <Link href="/courses">Courses</Link>
+        <Link href="/profile">My Profile</Link>
+      </div>
+
+      {/* Right */}
+      <div className="flex gap-2">
+        <Link href="/login" className="btn btn-outline btn-sm">
+          Login
+        </Link>
+        <Link href="/register" className="btn btn-primary btn-sm">
+          Register
+        </Link>
+      </div>
+    </div>
+  );
+}
