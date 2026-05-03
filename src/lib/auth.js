34664be6ef0cdce9488d3f119edc1678404e4f49
Sadiqunnabi17 <sadiqunnabi@gmail.com>
2026-05-03 10:23:20 +0600

    Fix auth with SQLite - email and Google login working

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,7 +1,22 @@
 import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const client = new MongoClient(process.env.MONGO_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+await client.connect();
+const db = client.db("skillsphere");
 
 export const auth = betterAuth({
   secret: process.env.BETTER_AUTH_SECRET,
+
+  database: mongodbAdapter(db),
 
   emailAndPassword: {
     enabled: true,
