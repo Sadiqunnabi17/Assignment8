32bbb8cb988fa280efe36bede3a2724aa3157f49
Sadiqunnabi17 <sadiqunnabi@gmail.com>
2026-05-05 17:00:55 +0600

    SQLite local MongoDB production

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,18 +1,27 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import Database from "better-sqlite3";
 
-const client = new MongoClient(process.env.MONGO_URI, {
-  serverSelectionTimeoutMS: 5000,
-  connectTimeoutMS: 5000,
-});
+const isProd = process.env.NODE_ENV === "production";
 
-await client.connect();
-const db = client.db("skillsphere");
+let database;
+
+if (isProd) {
+  const client = new MongoClient(process.env.MONGO_URI, {
+    serverSelectionTimeoutMS: 5000,
+    connectTimeoutMS: 5000,
+  });
+  await client.connect();
+  const db = client.db("skillsphere");
+  database = mongodbAdapter(db, { client });
+} else {
+  database = new Database("./database.db");
+}
 
 export const auth = betterAuth({
   secret: process.env.BETTER_AUTH_SECRET,
-  database: mongodbAdapter(db, { client }),
+  database,
   emailAndPassword: { enabled: true },
   socialProviders: {
     google: {
